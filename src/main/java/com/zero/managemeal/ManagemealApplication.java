cfbd6ba91aec9fa491dac9984a84b15c676e4aa8
abir <abir@test.com>
2020-07-22 17:15:36 +0600

    basic setup!

---
+++ src/main/java/com/zero/managemeal/ManagemealApplication.java
@@ -0,0 +1,13 @@
+package com.zero.managemeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagemealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagemealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero/managemeal/ManagemealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.managemeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagemealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
